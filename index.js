8e3df3978f345937f68ffe09bac163ea53bcc999
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-09 11:13:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let age = 40
+age = '40'
+console.log(age)
+alert(age)
+
+let yearBrithday = '1982'
+yearBrithday = 1982
+console.log(yearBrithday)
+alert(yearBrithday)
+
+let newGame = true
+newGame = null
+console.log(newGame)
+alert(newGame)
